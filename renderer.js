3a80d36a50929fb0f0cfc0b98b80ba3045a36fb3
lukasz_brainhub <lukasz@brainhub.pl>
2019-03-27 21:42:42 +0100

    Added fetching and rendering weather data with weather icons

--- renderer.js
+++ renderer.js
@@ -1,22 +1,30 @@
 const updateValue = (id, value) => {
-    if (value) {
-        document.getElementById(id).innerHTML = value;
-    }
+  if (value) {
+    document.getElementById(id).innerHTML = value;
+  }
 };
 
 const setImage = (weatherDescription) => {
-    document.getElementById('weatherImage').src = `assets/images/${weatherDescription}.png`;
+  document.getElementById('weatherImage').src = `assets/images/${weatherDescription}.png`;
 };
 
 const updateCurrentWeather = (weatherData) => {
-    if (weatherData) {
-        setImage(weatherData.description);
-        updateValue('currentDescription', weatherData.description);
-        updateValue('currentTempLow', weatherData.temperatureLow);
-        updateValue('currentTempHigh', weatherData.temperatureHigh && `- ${weatherData.temperatureHigh}`);
-        updateValue('currentWindLow', weatherData.windLow);
-        updateValue('currentWindHigh', weatherData.windHigh && `- ${weatherData.windHigh}`);
-    }
+  console.log('weatherData', weatherData);
+  if (weatherData) {
+    setImage(weatherData.description);
+    updateValue('currentDescription', weatherData.description);
+    updateValue('currentTempLow', weatherData.temperatureLow);
+    updateValue('currentTempHigh', weatherData.temperatureHigh && `- ${weatherData.temperatureHigh}`);
+    updateValue('currentWindLow', weatherData.windLow);
+    updateValue('currentWindHigh', weatherData.windHigh && `- ${weatherData.windHigh}`);
+  }
 };
 
-// exercise-1 code goes here...
+const getWeatherData = (city = 'Bielsko-biała') => {
+  return fetch(`https://pogodynka.ml/${encodeURI(city)}`)
+    .then(response => response.json());
+};
+
+
+getWeatherData()
+  .then(weatherData => updateCurrentWeather(weatherData.now));
